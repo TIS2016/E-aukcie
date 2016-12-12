f1cadbcf4e98f120f15b4e5cc5e6edd4b0a9229b
bilek5 <tami626@citromail.hu>
2016-12-12 17:57:09 +0100

    commit

--- screenshot/linux64/screenshot.sh
+++
@@ -1,3 +0,0 @@
-cd lib
-java -cp screenshot.jar:client-combined-3.0.1-nodeps.jar:commons-exec-1.3.jar:commons-io-2.5.jar:commons-logging-1.2.jar:gson-2.3.1.jar:guava-19.0.jar:httpclient-4.5.2.jar:httpcore-4.4.4.jar screenshot.ScreenshotGUI
-
