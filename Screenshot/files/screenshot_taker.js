ee17fe36ccb7b25851c549ec0e791e566c425304
bilek5 <tami626@citromail.hu>
2016-12-30 18:40:05 +0100

    adapted to website
    - zobrazuje sa už iba potrebná časť stránky
    - názov súboru sa automaticky nastaví podľa názvu aukcie uvedeného na
    stránke
    - výsledný súbor "PB Galanta , 1. kolo.png" pridaný do priečinku
    "embedded"

--- Screenshot/files/screenshot_taker.js
+++ Screenshot/files/screenshot_taker.js
@@ -1,9 +1,26 @@
+var elementsToHide = [
+	top.document.getElementById('auctionRuntimeForm:j_id207'),
+	top.document.getElementById('j_id177'),
+	top.document.getElementById('j_id176').querySelector('h2'),
+	top.document.getElementById('j_id327')
+];
+
+var outputFileName = top.document.getElementById('j_id176').querySelector('h3').innerHTML.match(/\"(.*)\"/)[1];
+
 function screenshot() {
-	html2canvas(top.document.body, {
+	elementsToHide.forEach(function(e) {
+		e.style.display = 'none';
+	});
+	
+	html2canvas(top.document.getElementById('j_id175'), {
 		onrendered: function(canvas) {
 			canvas.toBlob(function(blob) {
-				saveAs(blob, 'output.png');
+				saveAs(blob, outputFileName + '.png');
 			}, 'image/png');
-		}
+		},
+	});
+	
+	elementsToHide.forEach(function(e) {
+		e.style.display = '';
 	});
 }
